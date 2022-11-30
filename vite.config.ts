600ddf4802fe04bc33c7acee6660bb3cc8f281e1
Rafis Bikbov <dribblerafis@gmail.com>
2022-11-30 10:32:02 +0200

    Rollup analyzer added.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import analyzer from 'rollup-plugin-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    analyzer({ summaryOnly: true }),
+    // just for lint
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
